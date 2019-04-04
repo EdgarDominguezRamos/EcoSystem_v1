eb0d27e58dbc6355ed08e3dcebe7c8a6c6c2bf2b
EdgarDominguezRamos <1717110264@utectulancingo.edu.mx>
2019-04-04 09:35:02 -0600

    Interfaces diseñadas

---
+++ app/src/main/java/com/example/ecosystem/Activity_New_Post.java
@@ -0,0 +1,13 @@
+package com.example.ecosystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_New_Post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__new__post);
+    }
+}
